b22448aabb62eef02edaeb67190f649a6f045592
elvis jose rosales <elvismaracaibomedellin@hotmail.com>
2023-03-27 15:25:56 -0500

    Almacenamiento ShartPreference

---
+++ app/src/main/java/shared_preferences/elvis/almacenamiento_shart/MainActivity.java
@@ -0,0 +1,38 @@
+package shared_preferences.elvis.almacenamiento_shart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Button Guardar;
+    private EditText etUsuario;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etUsuario=(EditText) findViewById(R.id.etUsuario);
+
+        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        etUsuario.setText(preferences.getString("usuarios",""));
+
+    }
+
+    public void guardar(View view)
+    {
+        SharedPreferences preferencias = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor obj_editor = preferencias.edit();
+        obj_editor.putString("usuarios", etUsuario.getText().toString());
+        obj_editor.commit();
+        finish();
+    }
+}
